b39e7b5b6e76038bdbe7e73cdebada41f02db4c0
hbashar434 <h.bashar434@gmail.com>
2025-12-26 18:02:03 +0600

    From the Beginning

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { useAuthStore } from "../store/useStore";
 import { LoadingSpinner } from "../components/LoadingSpinner";
 import "../global.css";
 import "react-native-reanimated";
+import { Text, View } from "react-native";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,26 +24,20 @@ function RootLayoutNav() {
     checkAuth();
   }, []);
 
-  useEffect(() => {
-    if (isLoading) return;
-
-    const inAuthGroup = segments[0] === "(auth)";
-
-    // If authenticated and currently inside the auth group, send to tabs
-    if (isAuthenticated && inAuthGroup) {
-      router.replace("/(tabs)");
-    }
-  }, [isAuthenticated, isLoading, segments]);
-
   if (isLoading) {
     return <LoadingSpinner message="Loading..." />;
   }
 
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-    </Stack>
+    <View className="flex-1 items-center justify-center bg-gray-100 px-4">
+      <Text className="text-2xl font-bold text-blue-600 mb-2">
+        Welcome to Travel App
+      </Text>
+
+      <Text className="text-base text-gray-700 text-center">
+        This layout is styled using Tailwind (NativeWind)
+      </Text>
+    </View>
   );
 }
 
